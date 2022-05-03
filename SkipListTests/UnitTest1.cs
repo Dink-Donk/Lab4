e6f80b12ab6f2fab5309b303d92b60ec4097bf34
Vladimir Sirazov <GratefulDonk@bitbucket.org>
2022-05-03 21:21:26 +0300

    Inital

---
+++ SkipListTests/UnitTest1.cs
@@ -0,0 +1,140 @@
+using System;
+using Xunit;
+using SkipList2020;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SkipListTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CountIncreaseAfterAdding()
+        {
+            int n = 10;
+            var lib = new SkipList<int, int>();
+
+            for(int i=0; i<n; i++)
+            {
+                lib.Add(i, i);
+            }
+            Assert.Equal(n, lib.Count);
+        }
+        [Fact]
+        public void ItemsExistsAfterAdding()
+        {
+            var lib = new SkipList<int, int>();
+            var nums = new List<int>(new[] { 44, 22, 1 , 56, 3, 90, 31, 15, 26 });
+            int n = nums.Count;
+            for (int i = 0; i < n; i++)
+            {
+                lib.Add(nums[i], i);
+            }
+            nums.Sort();
+            int j = 0;
+            foreach(var pair in lib)
+            {
+                Assert.Equal(nums[j], pair.Key);
+                j++;
+            }
+            Assert.Equal(n, lib.Count);
+        }
+
+        [Fact]
+        public void RandomItemsExistsAfterAdding()
+        {
+            var lib = new SkipList<int, int>();
+            var nums = new HashSet<int>();
+            var rd = new Random();
+            int n = 100;
+            while (nums.Count < n)
+            {
+                nums.Add(rd.Next(1, n * 3));
+            }
+            foreach(var item in nums)
+            {
+                lib.Add(item,1);
+            }
+
+            var a = nums.ToList();
+            a.Sort();
+            int j = 0;
+            foreach (var pair in lib)
+            {
+                Assert.Equal(a[j], pair.Key);
+                j++;
+            }
+            Assert.Equal(n, lib.Count);
+        }
+
+        [Theory]
+        [InlineData(90)]
+        [InlineData(1)]
+        [InlineData(44)]
+        public void ContainsFindsExistingElement(int keyToFind)
+        {
+            var lib = new SkipList<int, int>();
+            var nums = new List<int>(new[] { 44, 22, 1, 56, 3, 90, 31, 15, 26 });
+            int n = nums.Count;
+            for (int i = 0; i < n; i++)
+            {
+                lib.Add(nums[i], i);
+            }
+            Assert.True(lib.Contains(keyToFind));
+        }
+
+        [Fact]
+        public void ContainsDoesNotFindElement()
+        {
+            var lib = new SkipList<int, int>();
+            var nums = new List<int>(new[] { 44, 22, 1, 56, 3, 90, 31, 15, 26 });
+            int n = nums.Count;
+            for (int i = 0; i < n; i++)
+            {
+                lib.Add(nums[i], i);
+            }
+            Assert.False(lib.Contains(-1));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(90)]
+        [InlineData(56)]
+        [InlineData(3)]
+        public void ElementWasRemoved(int keyToRemove)
+        {
+            var lib = new SkipList<int, int>();
+            var nums = new List<int>(new[] { 44, 22, 1, 56, 3, 90, 31, 15, 26 });
+            int n = nums.Count;
+            for (int i = 0; i < n; i++)
+            {
+                lib.Add(nums[i], i);
+            }
+            lib.Remove(keyToRemove);
+            Assert.False(lib.Contains(keyToRemove));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(90)]
+        [InlineData(56)]
+        [InlineData(3)]
+        public void RemoveRemovesOnlyOneElement(int keyToRemove)
+        {
+            var lib = new SkipList<int, int>();
+            var nums = new List<int>(new[] { 44, 22, 1, 56, 3, 90, 31, 15, 26 });
+            int n = nums.Count;
+            for (int i = 0; i < n; i++)
+            {
+                lib.Add(nums[i], i);
+            }
+            lib.Remove(keyToRemove);
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] == keyToRemove)
+                    continue;
+                Assert.True(lib.Contains(nums[i]));
+            }
+        }
+    }
+}
---
+++ SkipListTimeTests/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkipList2020;
+
+namespace SkipListTimeTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SkipList<int, int> ownCollection = new SkipList<int, int>();
+            SkipList<int, int> dummyCollection = new SkipList<int, int>();
+            SortedList<int, int> mcCollection = new SortedList<int, int>();
+            int n = 10000;
+            var elements = GenerateRandomIntegers(n);
+            var watch = new Stopwatch();
+            for (int i = 0; i < n; i++)
+                dummyCollection.Add(elements[i], 1);
+            for (int i = 5000; i <7000; i++)
+                dummyCollection.Remove(elements[i]);
+            for (int i = 0; i < n; i++)
+                dummyCollection.Contains(elements[i]);
+            watch.Restart();
+            for (int i = 0; i < n; i++)
+            {
+                mcCollection.Add(elements[i], elements[i]);
+            }
+            watch.Stop();
+            Console.WriteLine($"MC sorted list add time:{watch.ElapsedMilliseconds}");
+
+            watch.Restart();
+            for (int i = 5000; i <= 7000; i++)
+            {
+                mcCollection.Remove(elements[i]);
+            }
+            watch.Stop();
+            Console.WriteLine($"MC sorted list remove time:{watch.ElapsedMilliseconds}");
+
+            watch.Restart();
+            for (int i = 0; i < n; i++)
+            {
+                mcCollection.ContainsKey(elements[i]);
+            }
+            watch.Stop();
+            Console.WriteLine($"MC sorted list find time:{watch.ElapsedMilliseconds}");
+
+            watch.Restart();
+            for (int i = 0; i < n; i++)
+            {
+                ownCollection.Add(elements[i], elements[i]);
+            }
+            watch.Stop();
+            Console.WriteLine($"Skip list add time:{watch.ElapsedMilliseconds}");
+
+            watch.Restart();
+            for (int i = 5000; i <= 7000; i++)
+            {
+                ownCollection.Remove(elements[i]);
+            }
+            watch.Stop();
+            Console.WriteLine($"Skip list remove time:{watch.ElapsedMilliseconds}");
+
+            watch.Restart();
+            for (int i = 0; i < n; i++)
+            {
+                ownCollection.Contains(elements[i]);
+            }
+            watch.Stop();
+            Console.WriteLine($"Skip list find time:{watch.ElapsedMilliseconds}");
+
+
+        }
+
+        private static List<int> GenerateRandomIntegers(int count)
+        {
+            List<int> randomIntegers = new List<int>();
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            while (randomIntegers.Count < count)
+            {
+                int newRandomInteger = random.Next(-2 * count, 2 * count);
+
+                if (!randomIntegers.Contains(newRandomInteger))
+                    randomIntegers.Add(newRandomInteger);
+            }
+            Console.WriteLine("Integers generated");
+            return randomIntegers;
+        }
+
+    }
+}
